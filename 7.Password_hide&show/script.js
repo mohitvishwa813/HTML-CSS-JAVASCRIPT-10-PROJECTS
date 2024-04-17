ee7d824ed773ec748a63fa7be2e1d986a1a58d59
mohitvishwa813 <122216178+mohitvishwa813@users.noreply.github.com>
2024-04-17 10:19:54 -0700

    Add files via upload

---
+++ 7.Password_hide&show/script.js
@@ -0,0 +1,11 @@
+/*input ,eye*/
+const input=document.querySelector("input");
+const eye=document.querySelector("#eye-icon");
+eye.addEventListener("click",()=>{
+//   if(input.type==="password"){
+// input.type="text";}
+//   else{
+// input.type="password";}
+  input.type==='password'?(input.type='text'):(input.type='password');
+  
+});
